f510e809f50f53b43a596b66d15f988bc2ce0aee
FalseCoreCentury <simonov0101k@gmail.com>
2024-08-16 12:01:14 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,6 +13,15 @@ class TestCoin:
             (65, [0, 1, 1, 2]),
             (98, [3, 0, 2, 3]),
             (388, [3, 0, 1, 15])
+        ],
+        ids=[
+            "1 penny",
+            "1 nickel",
+            "1 dime",
+            "1 quarter",
+            "1 nickel, 1 dime, 2 quarters",
+            "3 pennies, 2 dimes, 3 quarters",
+            "3 pennies, 1 dime, 15 quarters"
         ]
     )
     def test_func_for_type_error(
